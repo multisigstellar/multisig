0559929ec6490b7d8ed22072019d3368ceafa4a3
multisigstellar <msigxlm@hotmail.com>
2017-01-03 07:37:18 +0000

    Add files via upload

---
+++ frontend/js/app/app.js
@@ -0,0 +1,108 @@
+
+var app = angular.module('multisig', 
+                        [ 
+                          'ui.router',
+                          'angularRandomString',
+                          'jsonFormatter',
+                          'multisigService'
+                          
+
+                        ]);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $provide) {
+
+ 
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+    .state('home', {
+      url: '/',
+      views:{
+          
+        'pgContainer' : {
+          templateUrl: 'js/app/tx/home.controller.html',
+          controller: 'homeController'
+        },
+            
+      },
+      data: {
+        requireLogin: false
+      }
+    })
+		.state('create', {
+			url: '/create',
+      views:{
+          
+        'pgContainer' : {
+          templateUrl: 'js/app/tx/create.controller.html',
+      	  controller: 'createController'
+      	},
+      			
+      },
+      data: {
+        requireLogin: false
+      }
+		})
+    .state('addsigners',{
+      url: '/addsigners',
+      params: {
+        txData: null,
+        tx_tag: null,
+        tx_xdr: null,
+        
+      },
+      views: {
+        'pgContainer': {
+          templateUrl: 'js/app/signers/addsigner.controller.html',
+          controller: 'addSignerController'
+        }
+      },
+      data:{
+        requireLogin: false
+      }
+    })    
+    .state('tx',{
+      url: '/tx/:tx_tag',
+      views: {
+        'pgContainer': {
+          templateUrl: 'js/app/tx/tx.controller.html',
+          controller: 'txController'
+        }
+      },
+      data:{
+        requireLogin: false
+      }
+    })
+    .state('sign',{
+      url: '/sign/:tx_tag',
+      views: {
+        'pgContainer': {
+          templateUrl: 'js/app/tx/sign.controller.html',
+          controller: 'signController'
+        }
+      },
+      data:{
+        requireLogin: false
+      }
+    });
+
+  $locationProvider.html5Mode(true);
+  
+
+});
+
+app.run(function ($rootScope, $state ) {
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+
+    $rootScope.siteURL = 'http://localhost/';
+    
+  });
+
+  $rootScope.$on("$stateChangeSuccess", function (event, toState, toParams) {
+
+    window.scrollTo(0, 0);
+
+});
+
+
+});
